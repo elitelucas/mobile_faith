5e462ce6a7973c287ab11d4bd0b66362ae8d935b
vladimir9255 <vovochkaperepelkin@yandex.ru>
2021-04-17 16:01:36 +0900

    4.17#theme

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,13 +2,8 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Skote
-            </div>
-            <div class="col-sm-6">
-                <div class="text-sm-right d-none d-sm-block">
-                    Design & Develop by Themesbrand
-                </div>
-            </div>
+                <script>document.write(new Date().getFullYear())</script> © FaithSpace
+            </div>            
         </div>
     </div>
 </footer>
